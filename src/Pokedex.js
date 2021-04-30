2e3918721b479a317efc6e72c48d077ac5f6fe17
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-04-30 10:57:58 -0300

    Exercicio 2 ✅

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -6,26 +6,44 @@ class Pokedex extends React.Component {
     super(props);
 
     this.state = {
-      index: 0
+      index: 0,
+      type: 'Fire',
+      lengthFilteredArr: 2,
     }
     this.changeIndex = this.changeIndex.bind(this);
+    this.changeType = this.changeType.bind(this);
   }
 
   changeIndex() {
+    const iterableIndex = this.state.lengthFilteredArr - 1;
     this.setState((prevState, _props) => ({
-      index: (prevState.index > 6) ? 0 : prevState.index + 1
+      index: (prevState.index === iterableIndex) ? 0 : prevState.index + 1
     }))
+  }
+
+  changeType({ target: { innerText }}, length) {
+    this.setState({
+      type: innerText,
+      lengthFilteredArr: length
+    })
   }
 
   
   render() {
     const { pokemons } = this.props;
-    const { index } = this.state;
-
+    const { index, type } = this.state;
+    const filteredPokemons = pokemons.filter((pokemon) => pokemon.type === type);
+    const lenghtFilteredArr = filteredPokemons.length;
     return (
-      <div className="pokedex">
-        <Pokemon key={ pokemons[index].id } pokemon={ pokemons[index] } />
-        <button onClick={ this.changeIndex }>Próximo pokemon</button>
+      <div>
+        <div className="pokedex">
+          <Pokemon key={ filteredPokemons[index].id } pokemon={ filteredPokemons[index] } />
+        </div>
+        <div className="buttons">
+          <button onClick={ this.changeIndex }>Próximo pokemon</button>
+          <button onClick={ (event) => this.changeType(event, lenghtFilteredArr) }>Fire</button>
+          <button onClick={ (event) => this.changeType(event, lenghtFilteredArr) }>Psychic</button>
+        </div>
       </div>
     );
   }
